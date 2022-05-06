f7ff4471e4072e8818e950018ccaad82bb6f2643
Michael Kendrick <michael@locumsmart.net>
2022-05-06 09:16:57 -0500

    finished

--- Develop/Routes/api_routes.js
+++ Develop/Routes/api_routes.js
@@ -1,17 +1,33 @@
 const router = require("express").Router();
 const path = require("path");
 const fs = require("fs");
+const { v1: uuidv1 } = require("uuid");
 
 router.get("/api/notes", (req, res) => {
   fs.readFile("./db/db.json", (err, results) => {
     if (err) {
       throw err;
     } else {
+      console.log("200");
       res.send(results);
     }
   });
 });
 
 //read the notes, json.parse, req.body, write db.json fs.writefile
+router.post("/api/notes", (req, res) => {
+  let newNote = req.body;
+  newNote.id = uuidv1();
+  fs.readFile("./db/db.json", function (err, data) {
+    var json = JSON.parse(data);
+    json.push(newNote);
+    fs.writeFile("./db/db.json", JSON.stringify(json), function (err) {
+      if (err) {
+        throw err;
+      }
+    });
+    res.json(newNote);
+  });
+});
 
 module.exports = router;
--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -67,7 +67,6 @@ const renderActiveNote = () => {
 };
 
 const handleNoteSave = () => {
-  console.log("this");
   const newNote = {
     title: noteTitle.value,
     text: noteText.value,
